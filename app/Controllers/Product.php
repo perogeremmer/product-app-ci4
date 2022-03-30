b7e27925b8fab89ea61efd0424fb734a2d19226c
perogeremmer <mhudyaramadhana@gmail.com>
2022-03-31 00:42:15 +0700

    Fix product CRUD

--- app/Controllers/Product.php
+++ app/Controllers/Product.php
@@ -3,28 +3,12 @@
 namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
+use App\Models\Product as ProductModel; 
 
 class Product extends ResourceController
 {
-    protected $session;
-
-    private $products = [
-        [
-            "id" => "623b476dc4f96", 
-            "name" => "Odol",
-            "category" => "utilities",
-            "stock" => 200,
-            "price" => 5000
-        ]
-    ];
-
     public function __construct() {
-        $this->session = \Config\Services::session();
-        $this->session->start();
-
-        if(!$this->session->get('products')) {
-            $this->session->set('products', $this->products);
-        }
+        $this->productModel = new ProductModel();
     }
 
     /**
@@ -34,8 +18,11 @@ class Product extends ResourceController
      */
     public function index()
     {
+        
+        $products = $this->productModel->findAll();
+
         $payload = [
-            "products" => $this->session->get('products')
+            "products" => $products
         ];
 
         echo view('product/index', $payload);
@@ -68,8 +55,6 @@ class Product extends ResourceController
      */
     public function create()
     {
-        $products = $this->session->get('products');
-
         $payload = [
             "id" => uniqid(),
             "name" => $this->request->getPost('name'),
@@ -78,9 +63,8 @@ class Product extends ResourceController
             "category" => $this->request->getPost('category'),
         ];
 
-        array_push($products, $payload);
 
-        $this->session->set('products', $products);
+        $this->productModel->insert($payload);
         return redirect()->to('/product');
     }
 
@@ -91,21 +75,13 @@ class Product extends ResourceController
      */
     public function edit($id = null)
     {
-        $products = $this->session->get('products');
-
-        $data = null;
-
-        foreach ($products as $item) {
-            if ($item['id'] == $id) {
-                $data = $item;
-            }
-        }
-
-        if (!$data) {
-            throw new \Exception("Data not found!");
+        $product = $this->productModel->find($id);
+        
+        if (!$product) {
+            throw new \Exception("Data not found!");   
         }
         
-        echo view('product/edit', ["data" => $data]);
+        echo view('product/edit', ["data" => $product]);
     }
 
 
@@ -116,30 +92,14 @@ class Product extends ResourceController
      */
     public function update($id = null)
     {
-        $products = $this->session->get('products');
-        $data = null;
+        $payload = [
+            "name" => $this->request->getPost('name'),
+            "stock" => (int) $this->request->getPost('stock'),
+            "price" => (int) $this->request->getPost('price'),
+            "category" => $this->request->getPost('category'),
+        ];
 
-        $_new_products = [];
-
-        foreach ($products as $item) {
-            if ($item['id'] == $id) {
-
-                $item['name'] = $this->request->getPost('name');
-                $item['category'] = $this->request->getPost('category');
-                $item['stock'] = (int) $this->request->getPost('stock');
-                $item['price'] = (int) $this->request->getPost('price');
-                
-                $data = $item;
-            }
-
-            array_push($_new_products, $item);
-        }
-
-        if (!$data) {
-            throw new \Exception("Data not found!");
-        }
-
-        $this->session->set('products', $_new_products);
+        $this->productModel->update($id, $payload);
         return redirect()->to('/product');
     }
 
@@ -150,25 +110,7 @@ class Product extends ResourceController
      */
     public function delete($id = null)
     {
-        $products = $this->session->get('products');
-        $data = null;
-
-        $_new_products = [];
-
-        foreach ($products as $item) {
-            if ($item['id'] == $id) {
-                $data = $item;
-                continue;  
-            }
-
-            array_push($_new_products, $item);
-        }
-
-        if (!$data) {
-            throw new \Exception("Data not found!");
-        }
-
-        $this->session->set('products', $_new_products);
+        $this->productModel->delete($id);
         return redirect()->to('/product');
     }
 }
